5c703132fd09f02d7d982bb10c4a6d4919b70c4a
Verena Ena Carpella <verenaparaluxe@gmail.com>
2018-06-16 15:20:28 +0200

    Fix: add table to unlogged view and clean edit and clear button from unlogged user

--- index.php
+++ index.php
@@ -68,6 +68,7 @@ if( isset($_POST['btn-login']) ) {
   <html>
     <head>
       <title>VerenaEnas Library</title>
+      <meta charset="utf-8">
       <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous"><!-- for the heart icon in the footer -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
@@ -90,10 +91,75 @@ if( isset($_POST['btn-login']) ) {
     </header>
     <section>
       <h1>This is our Big Library</h1>
-      <section class="cards">
+
+      <section class="action">
+        <h2>Please log in to see the Full List</h2>
+      </section>
+
+      <section>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="on" accept-charset="utf-8">
+          <?php
+            if ( isset($errMSG) ) {
+              echo $errMSG; }
+          ?>
+          <div class="form-group">
+            <label for="inputEmail">Email address</label>
+            <input type="email" name="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Your email" value="<?php echo $email; ?>" maxlength="40">
+            <span class="text-danger"><?php echo $emailError; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="inputPassword">Password</label>
+            <input type="password" name="pass" class="form-control" id="input" placeholder="Your Password" maxlength="15">
+            <span class="text-danger"><?php echo $passError; ?></span>
+          </div>
+          <button type="submit" name="btn-login" class="btn btn-primary">Log in</button>
+          <a class="registerText" href="register.php">Want to see full books list? <span class="signup">Sign up here</span></a>
+        </form>
+      </section>
+      <div class=" table-responsive-sm">
+        <table class='table table-sm table-hover table-dark'>
+          <thead class='thead-dark'>
+            <tr>
+            <th scope='col'>#ISBN</th>
+            <th scope='col'>image link</th>
+            <th scope='col'>title</th>
+            <th scope='col'>short description</th>
+            <th scope='col'>publishing date</th>
+            <th scope='col'>Type</th>
+            <th scope='col'>availibility</th>
+            </tr>
+          </thead>
+
+          <?php
+
+          $sql = "SELECT * FROM media WHERE media_type = 'Blue Ray DVD' && availibility='1'";
+          $result = $connect->query($sql);
 
 
-        <div class='card-group'>
+          if($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()) {
+                  echo
+                  "
+                    <tbody>
+                      <tr>
+                        <th scope='row'>".$row['ISBN']."</th>
+                        <td>".$row['img_src']."</td>
+                        <td>".$row['title']."</td>
+                        <td>".$row['short_descr']."</td>
+                        <td>".$row['pub_date']."</th>
+                        <td>".$row['media_type']."</td>
+                        <td>".$row['availibility']."</td>
+                      </tr>
+                    </tbody>";
+              }
+          } else {
+              echo "<p><center>No Data Avaliable</center></p>";
+          }
+          ?>
+        </table>
+      </div>
+        <section class="card-group">
+
           <?php
 
           $sql = "SELECT * FROM media WHERE media_type = 'Blue Ray DVD' && availibility='1'";
@@ -107,46 +173,21 @@ if( isset($_POST['btn-login']) ) {
 
                   echo
                   "
-                    <div class='card'>
-                      <img class='card-img-top' src='".$row['img_src']."' alt='Card image of the media' />
-                        <div class='card-body'>
-                          <h5 class='card-title'>".$row['title']."</h5>
-                          <p class='card-text'>Published ".$row['pub_date']." .".$row['short_descr']."</p>
-                          <p class='card-text'><small class='text-muted'>".$row['media_type']." with #".$row['ISBN']." is ".$row['availibility']." </small></p>
-                        </div>
-                        <a href='update.php?car_id=".$row['ISBN']."'><button type='button'>Edit</button></a>
-                        <a href='delete.php?car_id=".$row['ISBN']."'><button type='button'>Delete</button></a>
-                    </div>";
-              }
-          } else {
-              echo "<p><center>No Data Avaliable</center></p>";
-          }
-          ?>
-        </div>
-      </section>
-      <h2>Please log in to see the Full List</h2>
-    </section>
-    <section>
-      <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="on">
-        <?php
-          if ( isset($errMSG) ) {
-            echo $errMSG; }
-        ?>
-        <div class="form-group">
-          <label for="inputEmail">Email address</label>
-          <input type="email" name="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Your email" value="<?php echo $email; ?>" maxlength="40">
-          <span class="text-danger"><?php echo $emailError; ?></span>
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-          <label for="inputPassword">Password</label>
-          <input type="password" name="pass" class="form-control" id="input" placeholder="Your Password" maxlength="15">
-          <span class="text-danger"><?php echo $passError; ?></span>
-        </div>
-        <button type="submit" name="btn-login" class="btn btn-primary">Log in</button>
-        <a class="registerText" href="register.php">Want to order some books and have no User? <span class="signup">Sign up here</span></a>
-      </form>
-    </section>
+                      <div class='card'>
+                        <img class='card-img-top' src='".$row['img_src']."' alt='Card image of the media' />
+                          <div class='card-body'>
+                            <h5 class='card-title'>".$row['title']."</h5>
+                            <p class='card-text'>Published ".$row['pub_date']." .".$row['short_descr']."</p>
+                            <p class='card-text'><small class='text-muted'>".$row['media_type']." with ISBN #".$row['ISBN']." is ".$row['availibility']." </small></p>
+                          </div>
+                      </div>";
+                  }
+                } else {
+                  echo "<p><center>No Data Avaliable</center></p>";
+                }
+                ?>
+          </section>
+
   </body>
 </html>
 <?php ob_end_flush(); ?>
