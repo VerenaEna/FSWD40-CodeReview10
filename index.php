fc9bb2153be812529989d441edfaa98ca01f7e07
Verena Ena Carpella <verenaparaluxe@gmail.com>
2018-06-16 15:20:28 +0200

    Feat: Add styling css relation

--- index.php
+++ index.php
@@ -69,84 +69,9 @@ if( isset($_POST['btn-login']) ) {
       <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous"><!-- for the heart icon in the footer -->
       <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css" />
     </head>
-    <style>
-      html {
-        font-size: 90%;
-        background: linear-gradient(rgba(0, 0, 0, 0.45),rgba(0, 0, 0, 0.45)),
-          url("asset/img/erol-ahmed-books-unsplash.jpg") no-repeat center center fixed;
-        background-size: cover;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
-      }
-      body {
-        height: 50%;
-        font-size: 1rem;
-        position: relative;
-        /* display: flex;
-        justify-content: flex-end; */
-        color: white;
-        background-color: transparent;
-      }
-      header {
-        background-color: white;
-        height: 100%;
-        width: 100%;
-      }
-      main {
-        width:40%;
-      }
 
-      section {
-        padding: 2rem;
-      }
-      form {
-        background: rgba(0,0,0,.5);
-        display:flex;
-        flex-direction: column;
-        width: 230px;
-        max-width: 80%;
-        padding: 5px;
-      }
-      h2{
-        text-align: center;
-        text-transform: uppercase;
-        padding-bottom: 1rem;
-      }
-      .registerText {
-        padding: 5px 0 0 0;
-        font-size: .7rem;
-        font-weight: bold;
-        color: lightgrey;
-
-      }
-
-      #emailHelp {
-        font-size: .7rem;
-      }
-
-      .signup {
-        text-decoration: underline;
-      }
-
-      .text-muted {
-        color: white;
-      }
-
-      @media screen and (max-width:420px){
-        main {
-          width: 100%;
-        }
-        form {
-          max-width: 100%;
-        }
-        body {
-          width: 100%;;
-        }
-      }
-    </style>
   <body>
     <header class="header">
       <ul class="nav nav-pills">
